ed752723ec8acfc2a8a5808d42911de8bc5e7f50
AswinT <aswint2018@gmail.com>
2026-03-31 22:30:25 +0530

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,18 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
+    origin: [
+      "http://localhost:5173",                    
+      "http://localhost:3000",
+      "https://mini-udemy-frontend-2tikcsrvp-aswint777s-projects.vercel.app"   
+    ],
+    credentials: true,          
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
+  })
 );
+
+app.options("*", cors());
 
 connectDB();
 
